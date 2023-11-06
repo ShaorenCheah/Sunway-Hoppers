b7ed5825e517c609a515f0e6db3a3c56e090739b
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-11-07 02:02:10 +0800

    added footer

---
+++ includes/footer.inc.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" type="text/css" href="./styles/footer.css">
+
+<div class="row pt-5 pb-2 w-100 m-0 footer">
+    <!-- Navbar brand -->
+    <div class="col-md-1"></div>
+    <div class="col-md-3">
+        <div class="row d-flex justify-content-center">
+            <a class="navbar-brand h-auto p-0 mb-2" href="index.php">
+                <img src="images/logo/footer.png" class="img-fluid" alt="Logo" style="width:40%">
+            </a>
+        </div>
+
+
+    </div>
+    <!-- Navbar brand -->
+    <div class="col-md-3"></div>
+
+    <!-- Navbar and Navitems -->
+
+    <div class="col-md-5">
+        <div class="row d-flex justify-content-end mx-5 text-muted">
+            <div class="col-md-6 px-5">
+                <p class="mb-2"><b>Phone</b></h4>
+                <p style="font-size:0.8rem">+60 12 - 3456 789</p>
+            </div>
+            <div class="col-md-6 px-5">
+                <p class="mb-2"><b>Address</b></h4>
+                <p style="font-size:0.8rem">Duplex, Jalan PJS 11/26,<br>Bandar Sunway,<br>47500 Subang Jaya, Selangor.</p>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-end mx-5 text-muted">
+            <div class="col-md-6 px-5">
+                <p class="mb-2"><b>Email</b></h4>
+                <p style="font-size:0.8rem">sunwayhoppers@gmail.com</p>
+            </div>
+            <div class="col-md-6 px-5 d-flex justify-content-start align-items-center gap-4">
+                <a href="#" class="icons"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="icons"><i class="bi bi-youtube"></i></a>
+                <a href="#" class="icons"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="icons"><i class="bi bi-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <hr class="mx-2">
+    </div>
+
+    <div class="col-md-12 d-flex justify-content-center">
+        <p class="text-muted" style="font-size:0.625rem">
+            <copyright>© 2023 SunwayHoppers</copyright>
+        </p>
+    </div>
+</div>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+<link rel="stylesheet" type="text/css" href="./styles/navbar.css">
 
 <nav class="navbar shadow container w-100 my-3 mb-lg-0 py-2 px-5 header">
   <div class="col d-flex justify-content-start">
