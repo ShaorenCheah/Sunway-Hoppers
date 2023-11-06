f403a6a1d5da1fc03b1af362e6a8e9fb784ffd3f
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-11-06 22:58:15 +0800

    modified navbar

--- components.php
+++ components.php
@@ -10,7 +10,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="./styles/style.css">
   <!-- Include icon link here -->
-  <title>KiraMakan</title>
+  <title>SunwayHoppers</title>
 </head>
 
 <body>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,19 +1,23 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
 <nav class="navbar shadow container w-100 my-3 mb-lg-0 py-2 px-5 header">
-  <div class="col">
-    <a class="navbar-brand">Navbar</a>
+  <div class="col d-flex justify-content-start">
+    <a class="navbar-brand" href="#">
+      <img src="images/logo/nav.png" width="100">
+    </a>
   </div>
   <ul class="col navbar-nav w-100  d-flex flex-row justify-content-around">
     <li class="nav-item">
       <a class="nav-link" href="#">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Find Carpool</a>
+      <a class="nav-link" href="#"><i class="bi bi-geo-alt-fill"></i> Find Carpool</a>
     </li>
     <li class="nav-item">
       <a class="nav-link">Rewards</a>
     </li>
   </ul>
   <div class="col d-flex justify-content-end">
-    <button type="button" class="btn btn-primary shadow">Login</button>
+    <button type="button" class="btn btn-primary shadow login px-4">Login</button>
   </div>
 </nav>
