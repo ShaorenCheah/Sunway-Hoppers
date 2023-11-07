c3b2d379446a5eb348432be193452d4a5d8578c0
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-11-07 17:57:30 +0800

    added font weights

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,5 +1,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <link rel="stylesheet" type="text/css" href="./styles/navbar.css">
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;500;600;700&family=Open+Sans:wght@300;400;600&family=Spectral:wght@300&display=swap');
+</style>
 
 <nav class="navbar shadow container w-100 my-3 mb-lg-0 py-2 px-5 header">
   <div class="col d-flex justify-content-start">
