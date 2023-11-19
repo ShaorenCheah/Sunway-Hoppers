c5a3f0daeb65c3c673738378bd1bad2c3d967135
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-11-20 02:13:21 +0800

    fix date issue

--- includes/admin/dashboard.inc.php
+++ includes/admin/dashboard.inc.php
@@ -1,62 +1,65 @@
 <h2>Dashboard</h2>
+<div class="row">
+    <div class="col">
+        <div class="summary shadow">
+            <h5 class="text-muted mb-0">Total Users</h5>
             <div class="row">
-                <div class="col">
-                    <div class="summary shadow">
-                        <h5 class="text-muted mb-0">Total Users</h5>
-                        <div class="row">
-                            <span class="w-50">
-                                <p class="summary-text">50</p>
-                            </span>
-                            <span class="w-50 summary-icon-orange">
-                                <i class="bi bi-person-fill"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="summary shadow ">
-                        <h5 class="text-muted mb-0">Total Drivers</h5>
-                        <div class="row">
-                            <span class="w-50">
-                                <p class="summary-text">20</p>
-                            </span>
-                            <span class="w-50 summary-icon-green">
-                                <i class="bi bi-person-badge-fill"></i></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="summary shadow">
-                        <h5 class="text-muted mb-0">Total Carpools</h5>
-                        <div class="row">
-                            <span class="w-50">
-                                <p class="summary-text">20</p>
-                            </span>
-                            <span class="w-50 summary-icon-orange">
-                            <i class="fa-solid fa-car"></i> 
-                        </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="summary shadow">
-                        <h5 class="text-muted mb-0">Rewards Left</h5>
-                        <div class="row ">
-                            <span class="w-50">
-                                <p class="summary-text">20</p>
-                            </span>
-                            <span class="w-50 summary-icon-green">
-                                <i class="bi bi-gift-fill"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr style="margin-top: 2.5rem;">
-            <div class="row">
-                <h2 class="w-50">Carpools Today</h2>
-                <span class="w-50 d-flex justify-content-end align-items-start">
-                    <button class="btn btn-primary shadow px-4 date "><?php  echo date('d/m/Y') ?></button>
+                <span class="w-50">
+                    <p class="summary-text">50</p>
                 </span>
-                
+                <span class="w-50 summary-icon-orange">
+                    <i class="bi bi-person-fill"></i>
+                </span>
             </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="summary shadow ">
+            <h5 class="text-muted mb-0">Total Drivers</h5>
+            <div class="row">
+                <span class="w-50">
+                    <p class="summary-text">20</p>
+                </span>
+                <span class="w-50 summary-icon-green">
+                    <i class="bi bi-person-badge-fill"></i></span>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="summary shadow">
+            <h5 class="text-muted mb-0">Total Carpools</h5>
+            <div class="row">
+                <span class="w-50">
+                    <p class="summary-text">20</p>
+                </span>
+                <span class="w-50 summary-icon-orange">
+                    <i class="fa-solid fa-car"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="summary shadow">
+            <h5 class="text-muted mb-0">Rewards Left</h5>
+            <div class="row ">
+                <span class="w-50">
+                    <p class="summary-text">20</p>
+                </span>
+                <span class="w-50 summary-icon-green">
+                    <i class="bi bi-gift-fill"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+<hr style="margin-top: 2.5rem;">
+<div class="row">
+    <h2 class="w-50">Carpools Today</h2>
+    <span class="w-50 d-flex justify-content-end align-items-start">
+        <button class="btn btn-primary shadow px-4 date ">
+            <?php date_default_timezone_set("Asia/Kuala_Lumpur");
+            echo date('d/m/Y'); 
+            ?></button>
+    </span>
+
+</div>
