f2d9af85e380f35ac4c6d052d0cfed7f06a7eb24
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-12-12 20:26:47 +0800

    Updated add profile pic feature
    - Separated backend and front end with JS JSON fetch

--- includes/modals/addPicModal.inc.php
+++ includes/modals/addPicModal.inc.php
@@ -8,7 +8,7 @@
             <div class="justify-content-center d-flex">
               <img id="previewImage" src="<?php echo $_SESSION['user']['profilePic']; ?>" alt="Avatar" class="my-4" style="border-radius: 50%; height: 10rem; width: 10rem; border: 0.2rem solid var(--secondary); object-fit: cover;">
             </div>
-            <form id="profPicForm" method="post" enctype="multipart/form-data" action="./backend/updateProfPic.php">
+            <form id="profPicForm" method="post" enctype="multipart/form-data" >
               <div class="mb-4 px-4">
                 <input type="file" class="form-control" id="image" name="image" required onchange="previewImage()" accept=".jpg, .jpeg, .png">
                 <small class="form-small-text">*Only JPG, JPEG and PNG files are allowed.</small>
@@ -40,5 +40,23 @@ function previewImage() {
   reader.readAsDataURL(input.files[0]);
   document.getElementById('updatePicBtn').disabled = false;
 }
+
+document.getElementById('profPicForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData();
+  var fileField = document.querySelector('#image');
+
+  formData.append('image', fileField.files[0]);
+
+  fetch('./backend/profile/updateProfPic.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    alert(data.message);
+    location.reload();
+  })
+});
 </script>
 
