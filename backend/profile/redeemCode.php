80a8f4fca16136b673606aca2b94582dff53f6cc
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-12-13 14:01:35 +0800

    Updated carpoolHistory user side

--- backend/profile/redeemCode.php
+++ backend/profile/redeemCode.php
@@ -11,53 +11,71 @@ $passengerID = $data['passengerID'];
 
 // Check if code exists
 $sql = "SELECT * FROM carpool_passenger WHERE code = :code AND carpoolID = :carpoolID AND accountID = :passengerID";
-$stmt = $pdo -> prepare($sql);
-$stmt -> bindParam(":code", $code, PDO::PARAM_STR);
-$stmt -> bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
-$stmt -> bindParam(":passengerID", $passengerID, PDO::PARAM_STR);
-$stmt -> execute();
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(":code", $code, PDO::PARAM_STR);
+$stmt->bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
+$stmt->bindParam(":passengerID", $passengerID, PDO::PARAM_STR);
+$stmt->execute();
 $rowCount = $stmt->rowCount();
-$row = $stmt -> fetch(PDO::FETCH_ASSOC);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if($rowCount > 0){
+if ($rowCount > 0) {
   // Update status to completed
   $sql = "UPDATE carpool_passenger SET status = 'Completed' WHERE code = :code AND carpoolID = :carpoolID AND accountID = :passengerID";
-  $stmt = $pdo -> prepare($sql);
-  $stmt -> bindParam(":code", $code, PDO::PARAM_STR);
-  $stmt -> bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
-  $stmt -> bindParam(":passengerID", $passengerID, PDO::PARAM_STR);
-  $stmt -> execute();
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(":code", $code, PDO::PARAM_STR);
+  $stmt->bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
+  $stmt->bindParam(":passengerID", $passengerID, PDO::PARAM_STR);
+  $stmt->execute();
 
   // Award points to passenger
   $sql = "UPDATE user SET rewardPoints = rewardPoints + 20 WHERE accountID = :passengerID";
-  $stmt = $pdo -> prepare($sql);
-  $stmt -> bindParam(":passengerID", $passengerID, PDO::PARAM_STR);
-  $stmt -> execute();
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(":passengerID", $passengerID, PDO::PARAM_STR);
+  $stmt->execute();
 
   // Award points to driver
   $sql = "UPDATE user SET rewardPoints = rewardPoints + 50 WHERE accountID = :driverID";
-  $stmt = $pdo -> prepare($sql);
-  $stmt -> bindParam(":driverID", $_SESSION['user']['accountID'], PDO::PARAM_STR);
-  $stmt -> execute();
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(":driverID", $_SESSION['user']['accountID'], PDO::PARAM_STR);
+  $stmt->execute();
 
   // Update carpool points earned
   $sql = "UPDATE carpool SET pointsEarned = pointsEarned + 50 WHERE carpoolID = :carpoolID";
-  $stmt = $pdo -> prepare($sql);
-  $stmt -> bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
-  $stmt -> execute();
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
+  $stmt->execute();
 
-  $message = "Code Redeemed Successfully";
+  $message = "Code Redeemed Successfully. Points has been awared to both passenger and driver.";
 
-}else{
-  $message = "Invalid Code";
+  // Check if all passengers have redeemed code
+  $sql = "SELECT * FROM carpool_passenger WHERE carpoolID = :carpoolID AND status = 'Accepted'";
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
+  $stmt->execute();
+  $acceptedCount = $stmt->rowCount();
+
+  $sql = "SELECT * FROM carpool_passenger WHERE carpoolID = :carpoolID AND status = 'Completed'";
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
+  $stmt->execute();
+  $completedCount = $stmt->rowCount();
+
+  if($completedCount > $acceptedCount){
+    // Update carpool status to completed
+    $sql = "UPDATE carpool SET status = 'Completed' WHERE carpoolID = :carpoolID";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":carpoolID", $carpoolID, PDO::PARAM_STR);
+    $stmt->execute();
+  }
+
+} else {
+  $message = "Invalid Code.";
 }
 
 $response = [
-  'action' => 'newRequestModal',
   'carpoolID' => $carpoolID,
   'message' => $message
 ];
 
 echo json_encode($response);
-
-?>
