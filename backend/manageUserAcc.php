6c8f478db736e738421a37e86cff84da04bd129c
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-12-14 14:35:11 +0800

    add transaction and rollback

--- backend/manageUserAcc.php
+++ backend/manageUserAcc.php
@@ -7,52 +7,54 @@ $data = json_decode($formJSON, true);
 $action = $data['action'];
 $accountID = $data['accountID'];
 
-if ($action == "deleteUser") {
-  //first delete all carpool sessions created by the user
-  $query = "DELETE FROM carpool_passenger
-      WHERE carpoolID IN (
-      SELECT carpoolID
-      FROM carpool
-      WHERE accountID = :accountID);";
-  $stmt = $pdo->prepare($query);
-  $stmt->bindParam(':accountID', $accountID, PDO::PARAM_STR);
-  $stmt->execute();
+try {
+  $pdo->beginTransaction();
 
-  //delete all other records of this userID
-  $tablesToDeleteFrom = [
-    'application',
-    'carpool',
-    'carpool_passenger',
-    'redemption',
-    'notification', // two occurrences, for senderID and recipientID
-    'user',
-    'account'
-  ];
-
-  foreach ($tablesToDeleteFrom as $table) {
-    if ($table == 'notification') {
-      $stmt = $pdo->prepare("DELETE FROM notification WHERE senderID = :accountID1 OR recipientID = :accountID2");
-      $stmt->bindParam(':accountID1', $accountID, PDO::PARAM_STR);
-      $stmt->bindParam(':accountID2', $accountID, PDO::PARAM_STR);
-    } else {
-      $stmt = $pdo->prepare("DELETE FROM $table WHERE accountID = :accountID");
+  if ($action == "deleteUser") {
+      // first delete all carpool sessions created by the user
+      $stmt = $pdo->prepare("DELETE FROM carpool_passenger
+          WHERE carpoolID IN (
+              SELECT carpoolID
+              FROM carpool
+              WHERE accountID = :accountID);");
       $stmt->bindParam(':accountID', $accountID, PDO::PARAM_STR);
-    }
-    $stmt->execute();
-  }
+      $stmt->execute();
 
-  $success = true;
-  $message = "All records of this user are deleted.";
-} else {
-  $message = "Error Deleting User";
+      // delete all other records of this userID
+      $tablesToDeleteFrom = [
+          'application',
+          'carpool',
+          'carpool_passenger',
+          'redemption',
+          'notification', // two occurrences, for senderID and recipientID
+          'user',
+          'account'
+      ];
+
+      foreach ($tablesToDeleteFrom as $table) {
+          if ($table == 'notification') {
+              $stmt = $pdo->prepare("DELETE FROM notification WHERE senderID = :accountID1 OR recipientID = :accountID2");
+              $stmt->bindParam(':accountID1', $accountID, PDO::PARAM_STR);
+              $stmt->bindParam(':accountID2', $accountID, PDO::PARAM_STR);
+          } else {
+              $stmt = $pdo->prepare("DELETE FROM $table WHERE accountID = :accountID");
+              $stmt->bindParam(':accountID', $accountID, PDO::PARAM_STR);
+          }
+          $stmt->execute();
+      }
+
+      $success = true;
+      $message = "All records of this user are deleted.";
+  } 
+  // Commit the transaction
+  $pdo->commit();
+} catch (PDOException $e) {
+  // An error occurred, rollback the transaction
+  $pdo->rollBack();
   $success = false;
+  $message = "Error Deleting User: " . $e->getMessage();
 }
 
-$response = [
-  'success' => $success,
-  'action' => $action,
-  'message' => $message,
-];
-// Send a JSON response indicating success or failure
+// Output success or failure
+echo json_encode(['success' => $success, 'message' => $message]);
 
-echo json_encode($response);
